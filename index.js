71430e4f21348a7b2cb1754ffd06ff93b2dc947a
William Wherry <williamwherryiv@gmail.com>
2017-11-11 23:24:34 -0500

    update default port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.SERVER_PORT || 80
+const port = process.env.SERVER_PORT || 3000
 const message = process.env.MESSAGE || 'Hello World'
 app.get('/', (req, res) => {
   res.send(message)
