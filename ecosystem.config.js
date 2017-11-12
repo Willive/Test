b0f70cca53420e9763f3d0b9ff4c925642746301
William Wherry <williamwherry@Williams-MBP.hsd1.fl.comcast.net>
2017-11-11 20:46:17 -0500

    pm2 setup

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+      name: 'wweb',
+      script: './index.js'
+    }],
+    deploy: {
+      production: {
+        user: 'ubuntu',
+        host: 'ec2-54-173-55-82.compute-1.amazonaws.com',
+        key: '~/will-web.pem',
+        ref: 'origin/master',
+        repo: 'git@github.com:Willive/wweb.git',
+        path: '/home/ubuntu/wweb',
+        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      }
+    }
+  }
